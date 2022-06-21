d121d5ba9cf1550694979c04e281bc1f54ff4431
Ch4rse da 404 <107947345+Ch4rse404@users.noreply.github.com>
2022-06-21 15:55:30 -0300

    Add files via upload

---
+++ tool-croacia.py
@@ -0,0 +1,209 @@
+#private da Skull's society 404
+
+import os
+import time
+import sys
+
+#collors
+Azul = '\033[94m'
+Verde = '\033[92m'
+Amarelo = '\033[93m'
+Vermelho = '\033[91m'
+Fim = '\033[0m'
+
+#collors2
+w = "\033[90;1m"
+m = "\033[91;1m"
+h = "\033[92;1m"
+k = "\033[93;1m"
+b = "\033[94;1m"
+p = "\033[95;1m"
+a = "\033[96;1m"
+s = "\033[97;1m"
+
+#time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+#requisitos
+os.system('pkg install ruby cowsay toilet figlet')
+os.system('gem install lolcat')
+
+#banner
+os.system("clear")
+print '''               \033[94m.@@:       !@@@@:       .@@@
+     !@@:     .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.     :@@!
+   .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#
+  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  .#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.
+    :@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+     !@@@@@@@@@@@@@@:                    .@@@@@@@@@@@@@@!
+       @@#:                                         #@@\033[0m
+      \033[91m #.        ##@@@#@@@.        .@@@@@@#@@         !
+       #.        ###@@@@@#.        .##@@@@@@#         !
+       #.        ##@@@@@##.        .@@@@@###@         !
+       #.        #@@@@@@@#.        .#@@@@@@@@         !
+       #@#@@@#@@@         !@@@@@@@@!         @@@#@@@@@@
+       ##@@@@@@@@         !@#@@@@@@!         @@@@@@#@@@
+       ##@@@@@##@         !@##@#@@@!         @@@@@@@@@@
+       #.        !@@@#@#@@.        .@#@@@@#@@         !
+       #.        !@@@@@#@@.        .@@@@@@@@#         !
+       #.        !@#@#@@@@.        .@@@@@@@@#         !
+       :.        :########.        .#########         !
+       :@#@@@@@#@         !@@@@@#@#!         ###@@@###.
+        :#@#@@#@@         !@@@@@##@!         @@@@@@@@!
+         .#@@@#@@         !@@@##@@@!         ##@@@@#
+            !.   !@@@#@@@@.        .@@@@@#@@#   .#.
+              .# !@@@@@@@#.        .#@@@@#@##.#.
+                  :#@@@#@@.        .#@@@@@@!
+                         .##########:\033[0m
+                                           v 1.5 '''
+#letreiro
+os.system('toilet -f standard "TEAM" -F gay')
+os.system('toilet -f standard "404" -F gay')
+print '''
+\033[92m___________________________________________\033[0m
+\033[92m|                                          |\033[0m
+\033[92m|\033[0m      https://github.com/ch4rse404        \033[92m|\033[0m  \033[93m[ 1 ]\033[0m \033[92machar vulnerabilidades\033[0m
+\033[92m|__________________________________________|\033[0m
+\033[92m|                               |   Bem    |\033[0m  \033[93m[ 2 ]\033[0m \033[92mpassword hack\033[0m
+\033[92m|\033[0m  \033[93mCodded by\033[0m : ch4rse404        \033[92m|  vindo   \033[92m|\033[0m
+\033[92m|\033[0m  \033[93mTeam\033[0m : team 404              \033[92m|   esse   \033[92m|\033[0m  \033[93m[ 3 ] \033[92mTelas Fakes\033[0m
+\033[92m|\033[0m  \033[93mLema\033[0m : o enter faz a forca a \033[92m|  script  \033[92m|\033[0m
+\033[92m|\033[0m  uniao faz os hackers         \033[92m|  e dos   \033[92m|\033[0m  \033[93m[ 4 ] \033[92mattack ddos/dos\033[0m
+\033[92m|\033[0m                               \033[92m|   404    \033[92m|\033[0m    
+\033[92m|\033[0m  \033[93mSCRIPT PRIVATE DOS 404\033[0m       \033[92m|          \033[92m|\033[0m  \033[93m[ 5 ] \033[92minstalar todas\033[0m
+\033[92m|_______________________________|__________|\033[0m
+                                              \033[93m[ 6 ]\033[0m \033[92msair\033[0m
+'''
+opcao = int(input("\033[93m----> SUA ESCOLHA E : \033[0m"))
+
+if opcao == 1:
+    os.system("clear")
+    os.system('toilet -f standard "SCANNERS" -F gay')
+    os.system("figlet sqlmap")
+    os.system("git clone https://github.com/sqlmapproject/sqlmap")
+    os.system("figlet redhawk")
+    os.system("git clone https://github.com/Tuhinshubhra/RED_HAWK ")
+    os.system("figlet hunner")
+    os.system("git clone https://github.com/b3-v3r/Hunner")
+elif opcao == 2:
+    os.system("clear")
+    os.system('toilet -f standard "PASSW" -F gay')
+    os.system("figlet hunner")
+    os.system("git clone https://github.com/b3-v3r/Hunner")
+    os.system("figlet brutal")
+    os.system("git clone http://github.com/Screetsec/Brutal")
+    os.system("figlet brute force")
+    os.system("git clone https://github.com/IAmBlackHacker/Facebook-BruteForce")
+    os.system("figlet hydra")
+    os.system("git clone https://github.com/Gameye98/Black-Hydra")
+elif opcao == 3:
+    os.system("clear")
+    os.system('toilet -f standard "Telas Fakes" -F gay')
+    os.system("figlet socialsploit")
+    os.system("git clone https://github.com/Cesar-Hack-Gray/SocialSploit")
+    os.system("figlet cam dumper")
+    os.system("git clone https://github.com/LiNuX-Mallu/CAM-DUMPER.git")
+elif opcao == 4:
+    os.system("clear")
+    os.system('toilet -f standard "DDOS" -F gay')
+    os.system("figlet dios ddos")
+    os.system("git clone https://github.com/DiosDeLaWeb/Dios-DDos")
+    os.system("figlet torshammer")
+    os.system("git clone https://github.com/dotfighter/torshammer ")
+    os.system("figlet hunner")
+    os.system("git clone https://github.com/b3-v3r/Hunner")
+    os.system("figlet hammer")
+    os.system("git clone https://github.com/cyweb/hammer")
+elif opcao == 5:
+    os.system("cleaf")
+    os.system('toilet -f standard "TODAS" -F gay')
+    print "[ + ] Preparando para instalar as ferramentas"
+    time.sleep(2)
+    print "[ + ] Lendo arquivos"
+    time.sleep(2)
+    print "[ + ] Comecando"
+    time.sleep(1)
+    os.system("figlet figlet")
+    os.system("apt install figlet")
+    os.system('toilet -f standard "FERRAMENTAS" -F gay')
+    os.system('toilet -f standard "SKULLS" -F gay')
+    os.system("figlet dios ddos")
+    os.system("git clone https://github.com/DiosDeLaWeb/Dios-DDos")
+    os.system("figlet person dios")
+    os.system("git clone https://github.com/DiosDeLaWeb/Person-Dios")
+    os.system("figlet scripts dios")
+    os.system("git clone https://github.com/DiosDeLaWeb/Dios-Scripts")
+    os.system('toilet -f standard "FIM" -F gay')
+    os.system("figlet sqlmap")
+    os.system("git clone https://github.com/sqlmapproject/sqlmap")
+    os.system("figlet brute force")
+    os.system("git clone https://github.com/IAmBlackHacker/Facebook-BruteForce")
+    os.system("figlet lazymux")
+    os.system("git clone https://github.com/Gameye98/Lazymux")
+    os.system("figlet pack")
+    os.system("git clone https://github.com/3xploit666/packtermux")
+    os.system("figlet toolx")
+    os.system("git clone https://github.com/Rajkumrdusad/Tool-X")
+    os.system("figlet brutal")
+    os.system("git clone http://github.com/Screetsec/Brutal")
+    os.system("figlet redhawk")
+    os.system("git clone https://github.com/Tuhinshubhra/RED_HAWK")
+    os.system("figlet torshammer")
+    os.system("git clone https://github.com/dotfighter/torshammer")
+    os.system("figlet hunner")
+    os.system("git clone https://github.com/b3-v3r/Hunner")
+    os.system("figlet hydra")
+    os.system("git clone https://github.com/Gameye98/Black-Hydra")
+    os.system("figlet hammer")
+    os.system("git clone https://github.com/cyweb/hammer")
+    os.system("figlet socialsploit")
+    os.system("git clone https://github.com/Cesar-Hack-Gray/SocialSploit")
+    os.system("figlet cam dumper")
+    os.system("git clone https://github.com/LiNuX-Mallu/CAM-DUMPER.git")
+
+elif opcao == 6:
+    print "fechando ferramenta"
+    time.sleep(2)
+    exit()
+elif opcao == 404:
+    print "bem vindo ao tool croacia"
+    print "esse e um comando secreto"
+    print "feito apenas para membros"
+    print "da team 404"
+    print
+    print "[ 11 ] LISTA DE SCRIPTS"
+    print "[ 12 ] CURSO HACKING"
+else:
+    print "opcao invalida"
+    os.system("python2 tool-croacia.py")
+skull = int(input("digite sua escolha : "))
+if skull == 11:
+    print '''
+https://www.mediafire.com/file/te0jrz638eg78pa/lista_de_scripts_by_mr_pack_.txt/file
+'''
+
+elif skull == 12:
+    print ''' links de cursos
+        https://www.academia.edu/31929275/Mini_Curso_Hacker
+        
+        https://www.academia.edu/33747080/Manual_Hacker
+        
+        https://drive.google.com/file/d/1FCA91AJdk1ZDdPLENemFj5I_iaiuhqIl/view?usp=drivesdk
+        
+        https://drive.google.com/file/d/1F1sicLWUup2WKc9zWtUGGjM4MhvfS4mB/view?usp=drivesdk
+        
+        https://drive.google.com/file/d/1F6UU3-p45Ajb7sUBjFIAAAkxTP3L7LnW/view?usp=drivesdk
+        
+        https://www.mediafire.com/file/fvxxzeaj7xixcqk/DarkMuxcom.hspcx.darkmuxv1.0.apk/file
+ '''
+ 
+else:
+    print "opcao invalida"
+    os.system("python2 skulls.py")
